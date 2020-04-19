30a41e9587648d1ab6a221ad6b3dd54000f1f106
zgovind <zarkar.tech@gmail.com>
2020-04-19 21:45:40 +0530

    controller updated

--- src/main/java/com/aws/elastic/beanstalk/example/Controller.java
+++ src/main/java/com/aws/elastic/beanstalk/example/Controller.java
@@ -1,5 +1,6 @@
 package com.aws.elastic.beanstalk.example;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+	@GetMapping
 	public String start(){
-		return "Hello";
+		return "Hello DevOps";
 	}
 }
